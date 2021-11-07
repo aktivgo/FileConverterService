0fadd99fac216ea28a44d927238bdc84c3dfa27c
aktivgo <AktivGo01062001@gmail.com>
2021-11-07 19:38:46 +0300

    Add jar file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,18 @@
 import fileConverter.XmlToJson;
-import fileReader.GamesReader;
-import gameLibrary.Game;
+import org.jetbrains.annotations.NotNull;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+    public static void main(String @NotNull [] args) throws IOException, ParserConfigurationException, SAXException {
         if (args.length != 2) {
             throw new IllegalArgumentException("Некорректное количество аргументов");
         }
 
         XmlToJson converter = new XmlToJson();
-        converter.convert("games.xml", "developers.json");
-        System.out.println("Преобразование прошло успешно, результат сохранен в файл " + "developers.json");
+        converter.convert(args[0], args[1]);
+        System.out.println("Преобразование прошло успешно, результат сохранен в файл " + args[1]);
     }
 }

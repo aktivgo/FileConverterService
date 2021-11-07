9e4870c9cbf1f59a862464722e943fc9c5297430
aktivgo <AktivGo01062001@gmail.com>
2021-11-07 21:16:07 +0300

    Rename Main class

--- src/main/java/XmlToJsonConverter.java
+++ src/main/java/XmlToJsonConverter.java
@@ -5,13 +5,13 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
-public class Main {
+public class XmlToJsonConverter {
     public static void main(String @NotNull [] args) throws IOException, ParserConfigurationException, SAXException {
         if (args.length != 2) {
             throw new IllegalArgumentException("Некорректное количество аргументов");
         }
 
-        XmlToJson converter = new XmlToJson();
+        fileConverter.XmlToJson converter = new fileConverter.XmlToJson();
         converter.convert(args[0], args[1]);
         System.out.println("Преобразование прошло успешно, результат сохранен в файл " + args[1]);
     }
